c04166efa08dccadeefc57fd4e51c1516e070f10
Cameron Kurth <cameron.d.kurth@gmail.com>
2021-12-18 20:57:40 -0600

    Add `intraday/4` and `alpha_vantage_coin/1` to Cryptocurrencies

--- lib/alpha_vantage/cryptocurrencies.ex
+++ lib/alpha_vantage/cryptocurrencies.ex
@@ -49,6 +49,84 @@ defmodule AlphaVantage.Cryptocurrencies do
 
   @doc """
 
+  Returns the real-time price of AVC with the United States Dollar (USD) as the base currency.
+  The AVC price is streamed from the major decentralized exchange (DEX) Uniswap.
+  Please reference https://www.alphavantage.co/documentation/#avc-price for more detail.
+
+  ## Parameters
+
+  _Optional_ (accepted as a keyword list)
+
+  - `:datatype`
+
+    By default, `datatype: "map"`.
+    Strings `"map"`, `"json"`, and `"csv"` are accepted with the following specifications:
+    - `"map"` returns a map;
+    - `"json"` returns JSON format;
+    - `"csv"` returns a CSV (comma separated value) file string.
+
+  """
+  @spec alpha_vantage_coin(Keyword.t()) ::
+          {:error, String.t()} | {:ok, map} | {:ok, String.t()}
+  def alpha_vantage_coin(opts \\ []) do
+    params = [function: "AVC_PRICE"]
+    AlphaVantage.query(Keyword.merge(params, opts))
+  end
+
+  @doc """
+
+  Returns intraday time series (timestamp, open, high, low, close, volume) of the cryptocurrency specified, updated realtime.
+  Please reference https://www.alphavantage.co/documentation/#crypto-intraday for more detail.
+
+  ## Parameters
+
+  **Required**
+
+  - `:symbol`
+
+    The digital/crypto currency of your choice.
+    It can be any of the currencies in the [digital currency list](https://www.alphavantage.co/digital_currency_list/).
+    For example: `"BTC"`
+
+  - `:market`
+
+    The exchange market of your choice.
+     It can be any of the market in the [market list](https://www.alphavantage.co/physical_currency_list/).
+    For example: `"USD"`
+
+  - `:interval`
+
+    Time interval between two consecutive data points in the time series.
+    The following values are supported and accepted as strings: `"1min"`, `"5min"`, `"15min"`, `"30min"`, `"60min"`, `"daily"`, `"weekly"`, `"monthly"`
+
+  _Optional_ (accepted as a keyword list)
+
+  - `:datatype`
+
+    By default, `datatype: "map"`.
+    Strings `"map"`, `"json"`, and `"csv"` are accepted with the following specifications:
+    - `"map"` returns a map;
+    - `"json"` returns JSON format;
+    - `"csv"` returns a CSV (comma separated value) file string.
+
+  - `:outputsize`
+
+    By default, `outputsize: "compact"`.
+    Strings `"compact"` and `"full"` are accepted with the following specifications:
+    - `"compact"` returns only the latest 100 data points in the intraday time series;
+    - `"full"` returns the full-length intraday time series.
+    The `"compact"` option is recommended if you would like to reduce the data size of each API call.
+
+  """
+  @spec intraday(String.t(), String.t(), String.t(), Keyword.t()) ::
+          {:error, String.t()} | {:ok, map} | {:ok, String.t()}
+  def intraday(symbol, market, interval, opts \\ []) do
+    params = [function: "CRYPTO_INTRADAY", symbol: symbol, market: market, interval: interval]
+    AlphaVantage.query(Keyword.merge(params, opts))
+  end
+
+  @doc """
+
   Returns the daily historical time series for a digital currency (e.g., BTC) traded on a specific market (e.g., CNY/Chinese Yuan), refreshed daily at midnight (UTC).
   Prices and volumes are quoted in both the market-specific currency and USD.
   Please reference https://www.alphavantage.co/documentation/#currency-daily for more detail.
