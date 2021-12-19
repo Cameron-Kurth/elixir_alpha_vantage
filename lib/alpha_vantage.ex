3ffb01980f4e498a345469a572bb703196540f6c
Cameron Kurth <cameron.d.kurth@gmail.com>
2021-12-18 20:45:08 -0600

    Remove config constraints and improve response handling

--- lib/alpha_vantage.ex
+++ lib/alpha_vantage.ex
@@ -2,13 +2,8 @@ defmodule AlphaVantage do
   @moduledoc """
   A set of functions to fetch data from the [Alpha Vantage API](https://www.alphavantage.co/documentation/#) in flexible manner, as well as retrieve the valid inputs for the API.
   """
-  alias AlphaVantage.Config
 
-  @url Config.url()
-  @param_keys Config.param_keys()
-  @functions Config.functions()
-  @intervals Config.intervals()
-  @series_types Config.series_types()
+  alias AlphaVantage.Gateway
 
   @doc """
 
@@ -22,111 +17,8 @@ defmodule AlphaVantage do
   def query(params) when is_map(params), do: params |> Map.to_list() |> query
 
   def query(params) when is_list(params) do
-    recv_timeout = Application.get_env(:alpha_vantage, :recv_timeout)
-    datatype = Keyword.get(params, :datatype, "map")
-
-    request_datatype =
-      case datatype do
-        "map" -> "json"
-        _ -> datatype
-      end
-
-    params =
-      Keyword.put(params, :apikey, Application.get_env(:alpha_vantage, :api_key))
-      |> Keyword.put(:datatype, request_datatype)
-
-    cond do
-      valid_param_keys?(params) == false ->
-        {:error,
-         "Invalid parameter key(s) passed. Please verify your parameter list for correctness."}
-
-      valid_function?(params) == false ->
-        {:error, "Invalid function '#{Keyword.get(params, :function)}'."}
-
-      valid_interval?(params) == false ->
-        {:error, "Invalid interval '#{Keyword.get(params, :interval)}'."}
-
-      valid_series_type?(params) == false ->
-        {:error, "Invalid series type '#{Keyword.get(params, :series_type)}'."}
-
-      true ->
-        {:ok,
-         HTTPoison.get!(@url, [], params: params, recv_timeout: recv_timeout).body
-         |> decode!(datatype)}
-    end
+    Gateway.query(params)
   end
 
-  @doc """
-
-  Returns the list of parameter keys that are supported by Alpha Vantage.
-
-  """
-  @spec get_param_keys() :: list
-  def get_param_keys(), do: @param_keys
-
-  # Ensures the parameter keys are supported by Alpha Vantage.
-  defp valid_param_keys?(params) do
-    Keyword.keys(params)
-    |> Enum.all?(fn x -> x in @param_keys end)
-  end
-
-  @doc """
-
-  Returns the list of :function values that are supported by Alpha Vantage.
-
-  """
-  @spec get_functions() :: list
-  def get_functions(), do: @functions
-
-  # Ensures the :function value is supported by Alpha Vantage.
-  defp valid_function?(params) do
-    Keyword.get(params, :function) in @functions
-  end
-
-  @doc """
-
-  Returns the list of :interval values that are supported by Alpha Vantage.
-
-  """
-  @spec get_intervals() :: list
-  def get_intervals(), do: @intervals
-
-  # Ensures the :interval value is supported by Alpha Vantage.
-  defp valid_interval?(params) do
-    if Keyword.has_key?(params, :interval) do
-      Keyword.get(params, :interval) in @intervals
-    else
-      true
-    end
-  end
-
-  @doc """
-
-  Returns the list of :series_type values that are supported by Alpha Vantage.
-
-  """
-  @spec get_series_types() :: list
-  def get_series_types(), do: @series_types
-
-  # Ensures the :series_type value is supported by Alpha Vantage.
-  defp valid_series_type?(params) do
-    if Keyword.has_key?(params, :series_type) do
-      Keyword.get(params, :series_type) in @series_types
-    else
-      true
-    end
-  end
-
-  # Decodes the query response to match the :datatype option passed to query/1.
-  defp decode!(response, datatype) when is_atom(datatype) do
-    decode!(response, Atom.to_string(datatype))
-  end
-
-  defp decode!(response, datatype) do
-    case datatype do
-      "map" -> Jason.decode!(response)
-      "json" -> response
-      "csv" -> response
-    end
-  end
+  def query(_), do: {:error, "Invalid query."}
 end
---
+++ lib/alpha_vantage/gateway.ex
@@ -0,0 +1,79 @@
+defmodule AlphaVantage.Gateway do
+  @moduledoc """
+  Interface to the AlphaVantage API (https://www.alphavantage.co/documentation).
+  """
+
+  @url "https://www.alphavantage.co/query"
+
+  @type response ::
+          {:ok, payload :: map()}
+          | {:error, :params, message :: String.t()}
+          | {:error, :authentication, message :: String.t()}
+          | {:error, :server_error, status_code :: integer()}
+          | {:error, :timeout}
+          | {:error, any()}
+
+  @spec query(Keyword.t() | map) :: {:error, String.t()} | {:ok, map} | {:ok, String.t()}
+  def query(params) do
+    datatype = Keyword.get(params, :datatype, "map")
+
+    request_datatype =
+      case datatype do
+        "map" -> "json"
+        _ -> datatype
+      end
+
+    params =
+      params
+      |> Keyword.put(:apikey, api_key())
+      |> Keyword.put(:datatype, request_datatype)
+
+    HTTPoison.get(@url, [], params: params, recv_timeout: recv_timeout())
+    |> handle_response(datatype)
+  end
+
+  defp handle_response(response, datatype) do
+    case response do
+      {:ok, %{status_code: 200, body: body}} ->
+        handle_payload(body, datatype)
+
+      {_, %{status_code: 400, body: body}} ->
+        {:error, :params, body}
+
+      {_, %{status_code: 401, body: body}} ->
+        {:error, :authentication, body}
+
+      {_, %{status_code: status_code}} when status_code in 500..599 ->
+        {:error, :server_error, status_code}
+
+      {_, %{reason: :timeout}} ->
+        {:error, :timeout}
+
+      {_, response} ->
+        {:error, response}
+    end
+  end
+
+  defp handle_payload(response_body, datatype) do
+    case String.contains?(response_body, "Error Message") do
+      true -> {:error, Jason.decode!(response_body)["Error Message"]}
+      false -> {:ok, decode!(response_body, datatype)}
+    end
+  end
+
+  # Decodes the query response to match the :datatype option passed to query/1.
+  defp decode!(response_body, datatype) when is_atom(datatype) do
+    decode!(response_body, Atom.to_string(datatype))
+  end
+
+  defp decode!(response_body, datatype) do
+    case datatype do
+      "map" -> Jason.decode!(response_body)
+      "json" -> response_body
+      "csv" -> response_body
+    end
+  end
+
+  defp api_key(), do: Application.get_env(:alpha_vantage, :api_key)
+  defp recv_timeout(), do: Application.get_env(:alpha_vantage, :recv_timeout)
+end
